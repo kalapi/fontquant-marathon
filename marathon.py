68ca97630da0aefd68dfc8e317b49af6aa542ad6
Kalapi Gajjar <kalapi.bordawekar@gmail.com>
2023-12-21 14:35:08 -0800

    Minor updates to marathon.py

--- marathon.py
+++ marathon.py
@@ -5,19 +5,27 @@ from argparse import ArgumentParser
 from fontquant import quantify
 
 __doc__ = """
-This script should:
 
-1. Iterate over all the fonts in a given directory (CMD arg)
-2. Find the 'Regular' weight file
-3. Process it through font quant
+To-do
+
+1. Iterate over all the font files in a given directory
+2. Find the 'Regular' weights 
+3. Process them through fontquant to get weight/width metrics
 4. Save the results in a dictionary
 5. Process/output the results in .CSV format
 
 """
 
-def findRegularFilePaths(main_directory, file_extension, weight_filter="regular"):
+def findRegularFilePaths(main_directory, file_extension, weight_filter="regular", existing_csv_file=None):
     """docstring for findRegularFiles"""
     paths = []
+    
+    existing_fonts = set()
+    if existing_csv_file:
+        with open(existing_csv_file, mode='r') as csv_file:
+            reader = csv.DisctReader(csv_file)
+            existing_fonts = set(row['Font name'] for row in reader)
+            
     for root, dirs, files in os.walk(main_directory):
         for file in files:
             if file.lower().endswith(weight_filter + '.' + file_extension):
@@ -33,7 +41,6 @@ def checkWeight(file_path):
     value = results["appearance"]["weight"]["value"]
     if value:
         data = (file_path.split("/")[-2], value)
-        print(file_path.split("/")[-2])
     return data
 
 def save_to_csv(raw_data, csv_filename, secondary_field_name):
@@ -59,9 +66,10 @@ def main(args=None):
         all_paths = findRegularFilePaths(source_path, "ttf")
         if len(all_paths) > 0:
             for path in all_paths:
+                print("Processing {} ...".format(path.split("/")[-2]))
                 weight_data.append(checkWeight(path))
-    if weight_data:
-        save_to_csv(weight_data, file_path, 'Weight')
+                if weight_data:
+                    save_to_csv(weight_data, file_path, 'Weight')
     else:
         print("no path found")
 
